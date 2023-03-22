221ad61a4c5e90177e97ddd1be73802dd6a6fef5
byandrev <byandrev@gmail.com>
2023-03-21 21:10:56 -0500

    upload files

--- script/D.java
+++ script/D.java
@@ -1,48 +1,42 @@
 import java.util.Scanner;
 
-public class D
-{
-    public static void main(String []args){// 23n^3 + 11n + 2
-        Scanner sc = new Scanner(System.in);//1
+public class D {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
 
-        byte A = sc.nextByte();//1
-        byte B = sc.nextByte();//1
+        byte A = sc.nextByte();
+        byte B = sc.nextByte();
         byte matriz[][] = new byte[A][B];//1
-        //1
-        for(byte i=0; i<A; i++){//2+1+3n->6n*n -> 6n^2
-            //1
-            for(byte j=0; j<B; j++){//2+1 -> 3*n -> 3n
+
+        for(byte i=0; i<A; i++){
+            for(byte j=0; j<B; j++){
                 matriz[i][j] = sc.nextByte();//1
             }
         }
-        procesarMatriz(matriz);//1+17n^+1 -> 17n^2 + 2
-        imp(matriz);//1+9n^2+1 -> 9n^2 +2
+        procesarMatriz(matriz);
+        imp(matriz);
     }
 
-    public static void procesarMatriz(byte matriz [][]){// 17n^2 + 1
-        //1
-        for(byte i=0; i<matriz.length; i++){//2+1+14n -> 17n*n -> 17n^2
-            //1
-            for(byte j=0; j<matriz[i].length; j++){//2+2+2+2+2+2 ->14*n -> 14n
-                if(i%2==0){//2
-                    matriz[i][j] += 1;//2
+    public static void procesarMatriz(byte matriz [][]){
+        for(byte i=0; i<matriz.length; i++){
+            for(byte j=0; j<matriz[i].length; j++){
+                if(i%2==0){
+                    matriz[i][j] += 1;
                 }
                 if(j%2==0){//2
-                    matriz[i][j] += 2;//2
+                    matriz[i][j] += 2;
                 }
-                if(i%2!=0 && j%2!=0){//2
-                    matriz[i][j] -= 3;//2
+                if(i%2!=0 && j%2!=0){
+                    matriz[i][j] -= 3;
                 }
             }
         }
     }
 
-    public static void imp(byte matriz [][]){// 9n^2 + 1
-        //1
-        for(byte i=0; i<matriz.length; i++){//2+1+6n->9n*n -> 9n^2
-            //1
-            for(byte j=0; j<matriz[i].length; j++){//2+4*n->6n
-                System.out.print(""+matriz[i][j]+(j<matriz[i].length-1?" ":"\n"));//4
+    public static void imp(byte matriz [][]){
+        for(byte i=0; i<matriz.length; i++){
+            for(byte j=0; j<matriz[i].length; j++){
+                System.out.print(""+matriz[i][j]+(j<matriz[i].length-1?" ":"\n"));
             }
         }
     }
